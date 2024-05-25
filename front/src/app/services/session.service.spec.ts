5e6dfa9d74125141e057bebc29a1baa6a6a10955
Brice Grenard <brice.grenard@gmail.com>
2024-05-25 16:14:25 +0200

    Add unit test for session.service class.

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,16 +2,54 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import {SessionInformation} from "../interfaces/sessionInformation.interface";
 
 describe('SessionService', () => {
   let service: SessionService;
+  const session: SessionInformation = {
+    token: '',
+    type:'',
+    id: 1,
+    username: 'yoga@user.com',
+    firstName: '',
+    lastName: '',
+    admin: false
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
   });
 
-  it('should be created', () => {
+  it('should create the session service', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should by default not be logged-in', () => {
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+  it('should update isLogged and sessionInformation variables on logIn', () => {
+    service.logIn(session);
+
+    expect(service.isLogged).toBe(true);
+    expect(service.sessionInformation).toEqual(session);
+  });
+
+  it('should update isLogged and sessionInformation variables on logOut', () => {
+    service.logOut();
+
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+  it('should emit the correct value for $isLogged', () => {
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBe(service.isLogged);
+    });
+
+    service.logIn(session);
+    expect(service.isLogged).toBe(true);
   });
 });
