e9410fbceaffdc126b6fd98f4f059f6abc9de7ef
Brice Grenard <brice.grenard@gmail.com>
2024-05-25 18:19:54 +0200

    Add unit test for user service class.

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -3,20 +3,60 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {User} from "../interfaces/user.interface";
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingControllerMock: HttpTestingController;
+  let mockUser: User;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
-        HttpClientModule
-      ]
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [UserService]
     });
     service = TestBed.inject(UserService);
+    httpTestingControllerMock = TestBed.inject(HttpTestingController);
+
+    mockUser = {
+      id: 1,
+      email: 'yoga@user.com',
+      password: 'none!',
+      firstName: 'yoga',
+      lastName: 'yoga',
+      admin: false,
+      createdAt: new Date(),
+    };
   });
 
-  it('should be created', () => {
+  it('should create user service', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should retrieve a user by ID', () => {
+    const userId = '1';
+    service.getById(userId).subscribe(user => {
+      expect(user).toEqual(mockUser);
+    });
+
+    const req = httpTestingControllerMock.expectOne(`${service['pathService']}/${userId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
+  });
+
+  it('should delete a user by ID', () => {
+    const userId = '1';
+    service.delete(userId).subscribe(response => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpTestingControllerMock.expectOne(`${service['pathService']}/${userId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
+
 });
