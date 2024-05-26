1884c1ccf84ef753af32245773a4f5c3ef0e8353
Brice Grenard <brice.grenard@gmail.com>
2024-05-26 14:29:05 +0200

    Add unit test for Sessions/ListComponent.

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -4,26 +4,64 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
-
 import { ListComponent } from './list.component';
+import {of} from "rxjs";
+import {Session} from "../../interfaces/session.interface";
+import {SessionApiService} from "../../services/session-api.service";
+import {SessionInformation} from "../../../../interfaces/sessionInformation.interface";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
+  const mockSessions: Session[] = [{
+    id: 1,
+    name: 'yoga@user.com',
+    date: new Date(),
+    description: '',
+    users: [1,2],
+    teacher_id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+    {
+      id: 2,
+      name: 'yoga1@user.com',
+      date: new Date(),
+      description: '',
+      users: [1,2],
+      teacher_id: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }];
+
   const mockSessionService = {
     sessionInformation: {
-      admin: true
+      admin: true,
+      id: 1
     }
+  }
+  const mockSessionApiService= {
+    all: jest.fn().mockReturnValue(of(mockSessions))
   }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{ provide: SessionService, useValue: mockSessionService }]
+      imports: [
+        HttpClientModule,
+        MatCardModule,
+        MatIconModule,
+        RouterLink
+      ],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService },
+        { provide: ActivatedRoute, useValue: null }
+      ]
     })
-      .compileComponents();
+    .compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
@@ -32,5 +70,28 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set sessions$ on ngOnInit', () => {
+    expect(mockSessionApiService.all).toHaveBeenCalled();
+
+    component.sessions$?.subscribe(sessions => {
+      expect(sessions).toEqual(mockSessions);
+    });
+  });
+
+  it('should return session information as user', () => {
+    const sessionInfo: SessionInformation ={
+      token: '',
+      type: '',
+      id: 2,
+      username: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      admin: false
+    };
+    mockSessionService.sessionInformation = sessionInfo;
+
+    expect(component.user).toBe(sessionInfo);
   });
 });
